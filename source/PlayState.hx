e96bd9ab0407b83814ae57d81389bdfed368d87f
xmati <93664892+yt-xmati@users.noreply.github.com>
2022-05-02 03:30:43 +0200

    yeah

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -67,16 +67,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Screwed...', 0.2], //From 0% to 19%
+		['Why are you so bad?', 0.4], //From 20% to 39%
+		['Ehhh', 0.5], //From 40% to 49%
+		['Oof', 0.6], //From 50% to 59%
+		['Nice', 0.69], //From 60% to 68%
+		['Hell nah', 0.7], //69%
+		['Cool', 0.8], //From 70% to 79%
+		['Very Cool!', 0.9], //From 80% to 89%
+		['Epic!', 1], //From 90% to 99%
+		['Very Epic!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
 	public var modchartSprites:Map<String, ModchartSprite> = new Map<String, ModchartSprite>();
@@ -185,7 +185,7 @@ class PlayState extends MusicBeatState
 	public var camOther:FlxCamera;
 	public var cameraSpeed:Float = 1;
 
-	var dialogue:Array<String> = ['blah blah blah', 'coolswag'];
+	var dialogue:Array<String> = ['blah blah blah', 'coolswag', 'https://github.com/mayo78/PSYCHDISCUSSIONS/discussions/611'];
 	var dialogueJson:DialogueFile = null;
 
 	var halloweenBG:BGSprite;
